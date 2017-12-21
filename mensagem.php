62dba76780189f1b6f3714a05528213f3fe2045b
Victor Ferreira <victorcasfer@yahoo.com.br>
2017-12-21 15:17:33 -0200

    mais ajustes no formulario

--- mensagem.php
+++ mensagem.php
@@ -1,16 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "12345";
-$dbname = "clientes_padaria";
-
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $assunto = $_POST['assunto'];
 $mensagem = $_POST['mensagem'];
 
 // Create connection
+$servername = "localhost";
+$username = "root";
+$password = "12345";
+$dbname = "clientes_padaria";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 //ajustar o charset de comunicação entre a aplicação e o banco de dados
@@ -24,10 +24,19 @@ if ($conn->connect_error) {
 $sql = "INSERT INTO contatos (nome, email, assunto, mensagem) VALUES ('$nome', '$email', '$assunto', '$mensagem')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Mensagem enviada com sucesso!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    //mostra alerta de envio na mesma página
+    echo '<script type="text/javascript">
+    alert("Mensagem enviada!");
+    location.href="contatos.html";
+    </script>';
+
+    // enviar mensagem por email
+    $mensagem = wordwrap($mensagem,70);
+    mail("contatos@pointdopao.com.br", "Point do Pão - ".$assunto, $mensagem);
+
+  } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 
 $conn->close();
 
